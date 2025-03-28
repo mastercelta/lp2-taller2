19ea995c1e1e5d4dd6af0bb7ee91a5438dc8cacb
Mastercelta <juancamilo@bees.com.co>
2025-03-27 19:18:32 -0500

    Actualizado para generar la aplicacion flask

--- main.py
+++ main.py
@@ -18,4 +18,18 @@ pprint(productos)
 cursor.close()
 conexion.close()
 
+# aplicación
+app = Flask(__name__)
 
+# rutas
+@app.route('/')
+def ruta_raiz():
+  pass
+
+@app.route('/producto/<int:pid>')
+def ruta_producto(pid):
+  pass
+  
+# programa principal
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
